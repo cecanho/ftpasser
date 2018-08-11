0dc44a20b4ed6c16595e66b6d78d713de93591d4
Cristiano José Cecanho <cecanho@gmail.com>
2018-08-10 23:11:15 -0300

    Adição de arquivos

---
+++ js/rosario.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cristiano.cecanho on 22/06/2016.
+ */
+function trocaImagem(){
+    $num = document.getElementById("terco").valueOf();
+    $num++;
+
+    document.getElementById("terco").value = $num;
+
+
+}
